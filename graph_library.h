516dbb335b8da160d0650e4978a024ad0691abff
Aryan-Gaurav <aryangaurav0504@gmail.com>
2021-06-09 02:56:22 +0530

    Added method signatures for remaining classes and final graph class

--- graph_library.h
+++ graph_library.h
@@ -2,10 +2,14 @@
 #ifndef GRAPH_LIBRARY_H
 #define GRAPH_LIBRARY_H
 
-#include<map>
-#include<vector>
-#include<utility> //for using pairs
-#include<iostream>
+#include <map>
+#include <vector>
+#include <utility> //for using pairs
+#include <cstdint> // for fixed sized integers
+#include <iostream>
+
+#define INF 4e18 //infinity for comparison purposes in Dijkstra and Bellman Ford
+
 
 template<typename K,typename V> // <key, value> pair
 class gmap
@@ -27,6 +31,15 @@ struct traversal
     void print();
 };
 
+template<typename N, typename T>
+struct node_pair
+{
+        N from,to;
+        T dis;
+        node_pair();
+        void set_value(N, N, T);
+        void print();
+};
 
 template<typename N, typename E>
 class Base
@@ -55,7 +68,76 @@ public:
     size_t size();
     void add_node( N& );
     void add_node( std::vector<N>& );
+    std::vector<traversal<N,int64_t> > bfs(N&, int);
+    std::vector<traversal<N,int64_t> > dfs(N&, int);
 };
+
+template<typename N, typename E>
+class Directed_Graph :
+    public virtual Base<N, E>
+{
+    public:
+        bool is_DAG();
+        std::vector<vector<N> > SCC();
+        std::vector<N> Topo_Sort();
+};
+
+template<typename N, typename E>
+class Undirected_Graph:
+    public virtual Base<N, E>
+{
+    public:
+        std::pair <int64_t, std::vector<E> > Prims_MST();
+        std::pair <int64_t, std::vector<E> > Krushkal_MST();
+};
+
+
+template<typename N, typename E>
+class Unweighted_Graph:
+    public virtual Base<N, E>
+{
+    public:
+        void add_edge(N&, N&, E&);
+
+        std::vector <traversal<N,int> > single_source_shortest_path(N&);
+        std::vector <node_pair<N,int> > all_pair_shortest_path();
+};
+
+template<typename N, typename E>
+class Weighted_Graph:
+    public virtual Base<N, E>
+{
+    private:
+        template<typename T>
+            auto Dijkstra(int, std::function< T (E)> );   //total 2 types of each for float and int
+        template<typename T>
+            auto Bellman_Ford(int, std::function< T (E)> );
+    public:
+        void add_edge(N&, N&, E&);   //distinction of function so no abuse of both add edge methods
+        
+        
+        template<typename T>
+            auto single_source_shortest_path( N&, std::function< T (E)> );
+        template<typename T>
+            auto all_pair_shortest_path( std::function< T (E)> );
+        template<typename T>
+            bool is_neg_cycle( std::function< T (E)> );
+
+        
+};
+
+template<typename N=int, typename E=int, bool is_directed=0, bool is_weighted=0>
+class graph:
+    public virtual std::conditional<is_directed, Directed_Graph<N,E>, Undirected_Graph<N,E> >::type,
+    public virtual std::conditional<is_weighted, Weighted_Graph<N,E>, Unweighted_Graph<N,E> >::type
+{
+    public:
+        graph(int);
+        graph();
+        void remove_node(N&);
+        void remove_edge(N&, N&);
+};
+
 
 //Functions definations for gmap class
 
