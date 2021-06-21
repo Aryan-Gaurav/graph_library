70e54a7fa8a5faf35cc824ba5e96a7bf8fe59a42
jayantkishore <31219974+jayantkishore@users.noreply.github.com>
2021-06-21 14:14:53 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash 
-dst="/usr/include/c++/*"
-for d in $dst; do
+DIR="/usr/include/c++/"
+if [ -d $DIR ]; then
+  ### Take action if $DIR exists ###
+  echo "Copying header files in ${DIR}..."
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Error: ${DIR} not found. EXIT!"
+  exit 1
+fi
+for d in $DIR/*; do
   if [ -d "$d" ]; then
     sudo cp -a -u $(pwd)/source/. "$d"
   fi
 done
+echo "Success !"
